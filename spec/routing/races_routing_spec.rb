925fb7d7a951357d47bac75e466e10d686284097
Isaac Moldofsky <isaac.moldofsky@gmail.com>
2015-01-25 18:29:01 -0600

    failing route test for races -> events

--- spec/routing/races_routing_spec.rb
+++ spec/routing/races_routing_spec.rb
@@ -10,6 +10,10 @@ describe RacesController do
     expect(get: "/races").to route_to('races#index')
   end
 
+  it "gets all races for certain event" do
+    expect(get: "/races/event/5").to route_to("races#show", event_id: '1')
+  end
+
   context 'nested under meet' do
     it "routes to races index for meet" do
       expect(get: "/meets/1/races").to route_to('races#index', meet_id: '1')
